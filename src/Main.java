1d19a378f920cd07832c8a49a96a84d837e21f43
iqbalhaidr <13523111@std.stei.itb.ac.id>
2025-04-11 10:50:05 +0700

    feat: driver

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        Utility.CompressionParams p = Utility.getUserInput();
+        // ====== Konfigurasi parameter ======
+        String inputPath = p.inputPath; // Gambar input
+        String outputPath = p.outputPath; // Gambar hasil
+        int method = p.errorMethod; // Metode error: "variance", "mad", "maxdiff", atau "entropy"
+        double threshold = p.threshold; // Threshold error
+        int minBlockSize = p.minBlockSize; // Minimum luas blok dalam pixel
+
+        try {
+            long startTime = System.nanoTime();
+
+            // ====== 1. Parsing gambar ======
+            ImageParser parser = new ImageParser(inputPath);
+
+            // ====== 2. Bangun quadtree ======
+            QuadTreeBuilder2 builder = new QuadTreeBuilder2(parser, method, threshold, minBlockSize);
+            QuadTreeNode root = builder.build();
+
+            // ====== 3. Rekonstruksi gambar ======
+            BufferedImage reconstructed = QuadTreeReconstructor.reconstructImage(
+                    root, parser.getWidth(), parser.getHeight());
+
+            // ====== 4. Simpan hasil rekonstruksi ======
+            QuadTreeReconstructor.saveImage(reconstructed, outputPath);
+            System.out.println("Gambar berhasil disimpan sebagai: " + outputPath);
+
+            long endTime = System.nanoTime();
+            double execTimeMs = (endTime - startTime) / 1_000_000.0; // dalam ms
+
+            OutputReporter.report(inputPath, outputPath, execTimeMs, root);
+
+        } catch (IOException e) {
+            System.err.println("Terjadi kesalahan saat membaca/menyimpan gambar: " + e.getMessage());
+        }
+    }
+}
